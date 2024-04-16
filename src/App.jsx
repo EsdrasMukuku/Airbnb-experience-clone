11031b92197aa2cdf241753e6e0f52a6a1d2b4f3
EsdrasMukuku <esdrasbam0@gmail.com>
2024-04-15 20:49:11 -0500

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
-
-
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Cards from './components/Cards'
-import './App.css'
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Cards from "./components/Cards";
+import data from "./data"
+import "./App.css";
 
 function App() {
-
-
+  const cards = data.map(item => {
+    return (
+        <Cards 
+            key={item.id}
+            item={item}
+        />
+    )
+})
   return (
     <>
-    <Navbar/>
-    <Hero /> 
-    <Cards 
-      img="image 12.png"
-      rating="5.0"
-      reviewCount={6}
-      country="USA"
-      title="Life Lessons with Katie Zaferes"
-      price={136}
-      />
-      
+      <Navbar />
+      <Hero />
+      <section className="cards-list">
+                {cards}
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,18 +2,24 @@
 import Star from '../../public/assets/Star 1.png'
 
 export default function Card(sum) {
-    
+    let badgeText
+    if (sum.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (sum.location === "Online") {
+        badgeText = "ONLINE"
+    }
     return (
         <div className="card">
-        <img src={`../../public/assets/${sum.img}`} className="card--image" />
-        <div className="card--stats">
-            <img src={Star} className="card--star" />
-            <span>{sum.rating}</span>
-            <span className="gray">({sum.reviewCount}) • </span>
-            <span className="gray">{sum.country}</span>
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+            <img src={`../../public/assets/${sum.item.coverImg}`} className="card--image" />
+            <div className="card--stats">
+                <img src={Star} className="card--star" />
+                <span>{sum.item.stats.rating}</span>
+                <span className="gray">({sum.item.stats.reviewCount}) • </span>
+                <span className="gray">{sum.item.location}</span>
+            </div>
+            <p className="card--title">{sum.item.title}</p>
+            <p className="card--price"><span className="bold">From ${sum.item.price}</span> / person</p>
         </div>
-        <p>{sum.title}</p>
-        <p><span className="bold">From ${sum.price}</span> / person</p>
-    </div>
     )
 }
